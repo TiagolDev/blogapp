070a3898b6e89e9233d28673f11876ff5605ccd1
TiagolDev <oliveira.tsilva@hotmail.com>
2022-01-08 17:28:08 -0300

    remocao de trecho de codigo no app.js e alteracao no db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production'){
-  module.exports = {mongoURI: "mongodb+srv://deploy:WEMsiAuvWSMFNhPt@cluster0.jphny.mongodb.net/blogapp"} 
+  module.exports = {mongoURI: "mongodb+srv://clusterAnything.mongodb.net/blogapp?retryWrites=true&w=majority"} 
 }else{
   module.exports ={mongoURI: "mongodb://localhost/blogapp"}
 }
