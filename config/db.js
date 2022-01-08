91938c4dd4f6d98de35169969fa477dd6766090b
TiagolDev <oliveira.tsilva@hotmail.com>
2022-01-08 19:11:32 -0300

    Adiciona procfile

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production'){
-  module.exports = {mongoURI: "mongodb+srv://clusterAnything.mongodb.net/blogapp?retryWrites=true&w=majority"} 
-}else{ 
+  module.exports = {mongoURI: "mongodb+srv://deployman:deploymongo123456@cluster0.jphny.mongodb.net/blogapp?retryWrites=true&w=majority"} 
+}else{
   module.exports ={mongoURI: "mongodb://localhost/blogapp"}
 }
