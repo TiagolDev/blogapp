a6255164924bd0755c282e09b75a9ac06b8442be
TiagolDev <oliveira.tsilva@hotmail.com>
2022-01-08 15:59:37 -0300

    alteracao no db config

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production'){
-  module.exports = {mongoURI: "mongodb+srv://username:password@cluster0.jphny.mongodb.net/blogapp?retryWrites=true&w=majority"} 
+  module.exports = {mongoURI: "mongodb+srv://deploy:WEMsiAuvWSMFNhPt@cluster0.jphny.mongodb.net/blogapp?retryWrites=true&w=majority"} 
 }else{
   module.exports ={mongoURI: "mongodb://localhost/blogapp"}
 }
